2c0bb7f8d29d8e6ea9d98243fc79e789c8813dc0
mmussett <mmussett@tibco.com>
2019-09-28 15:39:34 +0100

    Changed to apt-get for both Docker and Docker Compose.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,10 @@ fi
 sed -i "s/\(^deb cdrom.*$\)/\#/g" /etc/apt/sources.list
 
 apt-get update && apt-get upgrade -y && apt-get install -y curl
-
-curl -fsSL https://get.docker.com -o get-docker.sh
-
-sh get-docker.sh
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce
+curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
