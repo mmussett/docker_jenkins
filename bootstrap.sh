3ca2c699688d0f7fb3351b925001446971a3fe9a
mmussett <mmussett@tibco.com>
2019-09-28 16:40:53 +0100

    Annotated script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,13 +12,27 @@ if [[ -x "$(command -v docker)" ]]; then
   exit 1
 fi
 
+# Remove any references to Debian CDROM repositories or next line will fail
 sed -i "s/\(^deb cdrom.*$\)/\#/g" /etc/apt/sources.list
 
-apt-get update && apt-get upgrade -y && apt-get install -y curl
+# Update/upgrade apt
+apt-get update && apt-get upgrade -y
+
+# Install dependencies for docker to run
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Download docker repository key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add docker repository key to apt
 add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
+
+# Update apt now we've added docker repository
 apt-get update
+
+# Install docker CE
 apt-get install -y docker-ce
+
+# Install docker compose
 curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
